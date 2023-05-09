8e0c3bc6f793dc7b27af614c108a8a023204d1ef
Graham Home <grahamhome333@gmail.com>
2023-05-09 11:55:55 -0400

    Add exponentiation support & cleanup

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-Regex 1: https://regex101.com/r/P4tTsd/1
-Regex 2: https://regex101.com/r/052T3S/2
-Strategy: Run regex 1, then run regex 2 on 2nd captured group using find_iter()
+# Wordy
+This library provides a function which solves math problems written in English. It is my solution to the Exercism.org "Wordy" exercise.
+## Usage
+See tests for usage examples, use `cargo test` to run tests.
+## Concepts reviewed
+- `lazy_static!`
+- `?` operator
+- `Regex` crate
--- src/lib.rs
+++ src/lib.rs
@@ -5,39 +5,37 @@ mod tests;
 pub fn answer(command: &str) -> Option<i32> {
     lazy_static! {
         static ref BASIC_PATTERN: Regex = Regex::new(
-            r"^What\sis\s(-?\d+)((?:\s(?:plus|minus|multiplied\sby|divided\sby)\s-?\d+)*)\?$"
+            // Match the entire string as a math problem
+            // Capture the initial value and all operators and operands together
+            r"^What\sis\s(-?\d+)((?:\s(?:plus|minus|multiplied\sby|divided\sby|raised\sto\sthe)\s-?\d+(?:(?:nd|rd|th|st])\spower)?)*)\?$"
         )
         .unwrap();
+        // Match all operators and operands in the math problem
+        // Capture each operator and operand
         static ref OPERATOR_OPERAND_PATTERN: Regex =
-            Regex::new(r"(plus|minus|multiplied\sby|divided\sby)\s(-?\d+)").unwrap();
+            Regex::new(r"(plus|minus|multiplied\sby|divided\sby|raised\sto\sthe)\s(-?\d+)(?:(?:nd|rd|th|st])\spower)?").unwrap();
     }
-    if let Some(captures) = BASIC_PATTERN.captures(command) {
-        if let Ok(mut result) = captures.get(1).unwrap().as_str().parse::<i32>() {
-            for operator_operand_capture in
-                OPERATOR_OPERAND_PATTERN.captures_iter(captures.get(2).unwrap().as_str())
-            {
-                let (operator, operand) = (
-                    operator_operand_capture.get(1).unwrap().as_str(),
-                    operator_operand_capture
-                        .get(2)
-                        .unwrap()
-                        .as_str()
-                        .parse::<i32>()
-                        .unwrap(),
-                );
-                match operator {
-                    "plus" => result += operand,
-                    "minus" => result -= operand,
-                    "multiplied by" => result *= operand,
-                    "divided by" => result /= operand,
-                    _ => panic!(),
-                }
-            }
-            return Some(result);
-        } else {
-            return None;
+    let captures = BASIC_PATTERN.captures(command)?;
+    let mut result = captures.get(1)?.as_str().parse::<i32>().ok()?;
+    for operator_operand_capture in
+        OPERATOR_OPERAND_PATTERN.captures_iter(captures.get(2)?.as_str())
+    {
+        let (operator, operand) = (
+            operator_operand_capture.get(1)?.as_str(),
+            operator_operand_capture
+                .get(2)?
+                .as_str()
+                .parse::<i32>()
+                .ok()?,
+        );
+        result = match operator {
+            "plus" => result + operand,
+            "minus" => result - operand,
+            "multiplied by" => result * operand,
+            "divided by" => result / operand,
+            "raised to the" => result.pow(operand.try_into().ok()?),
+            _ => return None,
         }
-    } else {
-        return None;
     }
+    return Some(result);
 }
